24c835b31f0e0a9dc9a06ef7a7b9fdab7f289092
Lakshamana <lakshamana@protonmail.com>
2019-08-24 14:20:50 -0300

    add GET /users test

---
+++ test/backend/users.spec.js
@@ -0,0 +1,37 @@
+/**
+ * @jest-environment node
+ */
+
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+
+const { knex } = require('../../server/db')
+const { createSeeds, wipeTable } = require('../utils')
+const server = require('../../server')
+const HttpCodes = require('../../server/httpCodes')
+
+chai.use(chaiHttp)
+jest.useFakeTimers()
+
+// Suíte de testes da entidade users
+describe('prefix /api/users', () => {
+  beforeAll(async () => {
+    await knex.migrate.latest()
+  }, 100000)
+
+  beforeEach(async () => {
+    await createSeeds('users')
+  }, 100000)
+
+  afterEach(async () => {
+    await wipeTable(knex)
+  }, 100000)
+
+  test('GET /', async done => {
+    const response = await chai.request(server.listen()).get('/api/users')
+    expect(response.status).toBe(HttpCodes.OK.code)
+    expect(response.type).toBe('application/json')
+    expect(response.body).toBeDefined()
+    done()
+  })
+})
