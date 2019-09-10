b3e40a342fb4134ec0a2e55312e0dfde7b329e63
Lakshamana <guitrompa1@gmail.com>
2019-09-10 10:46:44 -0300

    turn catalog card to a function

--- server/models/pdfdocs/catalogCard.js
+++ server/models/pdfdocs/catalogCard.js
@@ -1,73 +1,94 @@
-const PDFDocument = require('pdfkit')
+/**
+ * Gera o modelo PDF baseado nos dados da
+ * ficha catalográfica gerados pelo usuário final
+ * @param {PDFKit.PDFDocument} doc
+ * @param {Object} {
+ *  cutter: String
+ *  writer: {
+ *    name: String,
+ *    surname: String,
+ *    course: String,
+ *    institute: String
+ *  },
+ *  advicer: {name: String, surname: String, title: String},
+ *  coadvicer: {name: String, surname: String, title: String},
+ *  work: {
+ *    title: String,
+ *    subtitle: String,
+ *    year: String,
+ *    totalPages: String,
+ *    type: String
+ *  },
+ *  keywords: String[],
+ *  cdd: String
+ * }
+ */
+function catalogCard(
+  doc,
+  { cutter, writer, advicer, coadvicer, work, keywords, cdd }
+) {
+  doc.moveDown(20)
 
-const doc = new PDFDocument()
-doc.moveDown(20)
-
-const header = `
+  const header = `
 Dados Internacionais de Catalogação na Publicação (CIP) de acordo com ISBD
 Sistema de Bibliotecas da Universidade Federal do Pará
 Gerada automaticamente pelo módulo Ficat, mediante os dados fornecidos pelo(a) autor(a)`
 
-doc.font('Times-Bold', 10).text(header, {
-  align: 'center',
-  continued: true
-})
+  doc.font('Times-Bold', 10).text(header, {
+    align: 'center',
+    continued: true
+  })
 
-function drawLine(doc, x, y, length) {
+  function drawLine(doc, x, y, length) {
+    doc
+      .lineWidth(2)
+      .moveTo(x, y)
+      .lineTo(x + length, y)
+      .stroke()
+  }
+
+  drawLine(doc, 90, 425, 430)
+
   doc
-    .lineWidth(2)
-    .moveTo(x, y)
-    .lineTo(x + length, y)
-    .stroke()
+    .font('Times-Roman', 10)
+    .text(cutter, -270, 450, {
+      align: 'justify'
+    })
+    .text(` ${writer.surname}, ${writer.name}`, 150, 450)
+    .text(`   ${work.title} - ${writer.name} ${writer.surname}. — ${work.year}`)
+    .text(` ${work.totalPages} f.`)
+
+  const advicerHeader = `Orientador(a): ${advicer.title} ${advicer.name} ${advicer.surname}`
+  const coadvicerHeader = `Coorientador(a): ${coadvicer.title} ${coadvicer.name} ${coadvicer.surname}`
+
+  const workHeader = `${work.type} - ${writer.course}`
+
+  const localHeader = `${writer.institute}, Universidade Federal do Pará, Belém, 2019.`
+
+  let kws = ''
+  for (const kn in keywords) {
+    kws += `${kn + 1}. ${keywords[kn]}`
+  }
+  const keywordHeader = `${kws} . I. ${work.title}.`
+
+  doc
+    .moveDown(1)
+    .text('   ' + advicerHeader)
+    .text('   ' + coadvicerHeader)
+    .text('   ' + workHeader, {
+      width: 300
+    })
+    .text(localHeader, {
+      width: 300
+    })
+    .moveDown(0.5)
+    .text(keywordHeader, {
+      width: 300
+    })
+
+  doc.moveDown(1).text(cdd, 520 - cdd.length * 5.5)
+
+  drawLine(doc, 90, 610, 430)
 }
 
-drawLine(doc, 90, 425, 430)
-
-const incognito = 's677t'
-const writerName = 'NOME'
-const writerSurname = 'SOBRENOME'
-const title = 'TÍTULO'
-const year = 2019
-const pageNumber = 'NÚMERO DE FOLHAS'
-
-doc
-  .font('Times-Roman', 10)
-  .text(incognito, -270, 450, {
-    align: 'justify'
-  })
-  .text(` ${writerSurname}, ${writerName}`, 150, 450)
-  .text(`   ${title} - ${writerName} ${writerSurname}. — ${year}`)
-  .text(` ${pageNumber} f.`)
-
-const advicerName = 'Orientador'
-const advicerSurname = 'Sobrenome do Orientador'
-const advicerHeader = `Orientador(a): Prof. Dr. ${advicerName} ${advicerSurname}`
-
-const course =
-  'Licenciatura Integrada em Educação em Ciências, Matemáticas e Linguagens'
-const workType = 'Trabalho de Conclusão de Curso (Graduação)'
-const workHeader = `${workType} - ${course}`
-
-const instituteName = 'Instituto de Educação Matemática e Científica'
-const localHeader = `${instituteName}, Universidade Federal do Pará, Belém, 2019.`
-
-const kw = ['ASDASDASD']
-const keywordHeader = `1. ${kw[0]} . I. Título.`
-
-doc
-  .moveDown(1)
-  .text('   ' + advicerHeader)
-  .text('   ' + workHeader, {
-    width: 300
-  })
-  .text(localHeader, {
-    width: 300
-  })
-  .moveDown(0.5)
-  .text(keywordHeader)
-
-const cdd = 'CDD 004'
-
-doc.moveDown(1).text(cdd, 430 + 90 - cdd.length * 5.5)
-
-drawLine(doc, 90, 610, 430)
+module.exports = catalogCard
