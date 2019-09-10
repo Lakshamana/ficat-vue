c4221850c4dc5d893d2621a6e0595fef5e5125c1
Lakshamana <guitrompa1@gmail.com>
2019-09-10 10:45:49 -0300

    add romanize

--- shared/utils.js
+++ shared/utils.js
@@ -95,4 +95,39 @@ function maybe(key, value) {
   return value && { [key]: value }
 }
 
-module.exports = { validatePayload, select, maybe }
+/**
+ * 21 => uppercase ? XXI : xxi
+ * @param {Number} n
+ * @param {Boolean} uppercase
+ */
+function romanize(n, uppercase = false) {
+  let r = ''
+  const romans = {
+    1: 'i',
+    4: 'iv',
+    5: 'v',
+    9: 'ix',
+    10: 'x',
+    40: 'xl',
+    50: 'l',
+    90: 'xc',
+    100: 'c',
+    400: 'cd',
+    500: 'd',
+    900: 'cm',
+    1000: 'm'
+  }
+
+  while (n > 0) {
+    let choice
+    for (const r in romans) {
+      if (n >= r) choice = r
+      else break
+    }
+    r += romans[choice]
+    n -= choice
+  }
+  return uppercase ? r.toUpperCase() : r
+}
+
+module.exports = { validatePayload, select, maybe, romanize }
