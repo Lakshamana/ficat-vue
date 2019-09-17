86f18088b98f5984b4fcb0919945f317be8c34b4
Lakshamana <guitrompa1@gmail.com>
2019-09-17 11:33:31 -0300

    create about page

---
+++ pages/about.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="section center">
+    <div class="columns is-centered is-mobile">
+      <div class="column is-10">
+        <card title="Sobre FICAT">
+          <div class="columns is-left">
+            <div class="column has-text-justified">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in
+                diam massa. Nam viverra condimentum metus consectetur auctor.
+                Vestibulum faucibus suscipit ornare. Aliquam erat volutpat.
+              </p>
+
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in
+                diam massa. Nam viverra condimentum metus consectetur auctor.
+                Vestibulum faucibus suscipit ornare. Aliquam erat volutpat.
+              </p>
+
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in
+                diam massa. Nam viverra condimentum metus consectetur auctor.
+                Vestibulum faucibus suscipit ornare. Aliquam erat volutpat.
+              </p>
+            </div>
+          </div>
+        </card>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import Card from '@/components/Card'
+
+export default {
+  name: 'About',
+
+  components: {
+    Card
+  },
+
+  computed: {
+    ...mapState({
+      lang: state => state.lang.langTag
+    })
+  }
+}
+</script>
