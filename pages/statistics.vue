0ea8c8b72335eae6bc3badbcfdd46e2c0e730320
Lakshamana <guitrompa1@gmail.com>
2019-10-07 11:18:01 -0300

    init statistics page

--- pages/statistics.vue
+++ pages/statistics.vue
@@ -1,12 +1,24 @@
 <template>
-  <div class="container">
-    I am the statistics page.
+  <div class="container is-fullwidth">
+    <section class="section columns">
+      <aside class="column is-3">
+        <p class="menu-label is-hidden-touch">
+          Filtros
+        </p>
+        <div class="navbar-menu has-background-white">
+          <div class="navbar-start"></div>
+        </div>
+      </aside>
+      <div class="column is-9 is-right">
+        I am the Statistics page.
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Statistics',
-  middleware: 'auth'
+  layout: 'empty'
 }
 </script>
