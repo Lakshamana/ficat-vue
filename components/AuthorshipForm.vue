b5fcb3bad7f935a799e1790824fb622adc442ee5
Lakshamana <guitrompa1@gmail.com>
2020-01-28 23:13:47 -0300

    creating AuthorshipForm

---
+++ components/AuthorshipForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <Card title="Authorship Data">
+    <div class="columns">
+      <div class="column is-half">
+        <input-validation
+          v-model="authorName"
+          :properties="authorNameProperties"
+          class="mb"
+        ></input-validation>
+        <input-validation
+          v-model="authorSurname"
+          :properties="authorSurnameProperties"
+        ></input-validation>
+      </div>
+      <div class="column is-half">
+        <input-validation
+          v-model="author2Name"
+          :properties="author2NameProperties"
+        ></input-validation>
+        <input-validation
+          v-model="author2Surname"
+          :properties="author2SurnameProperties"
+        ></input-validation>
+      </div>
+    </div>
+  </Card>
+</template>
+
+<script>
+import Card from '~/components/Card'
+export default {
+  name: 'AuthorshipForm',
+  components: { Card },
+  data() {
+    return {
+      authorName: '',
+      authorSurname: '',
+      author2Name: '',
+      author2Surname: ''
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ components/Slider.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="section center">
+    <div class="columns is-centered is-mobile">
+      <div class="column is-10">
+        <keep-alive>
+          <component :is="slides[idx]" />
+        </keep-alive>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    startIndex: {
+      type: Number,
+      default: 0
+    },
+    slides: {
+      type: Array,
+      default: () => []
+    }
+  },
+
+  data() {
+    return {
+      idx: this.startIndex
+    }
+  }
+}
+</script>
