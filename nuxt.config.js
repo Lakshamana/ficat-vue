6797263c07dbfe744b073206ed5a9265415c520a
Lakshamana <guitrompa1@gmail.com>
2020-01-28 23:13:25 -0300

    add vuelidate

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,7 @@ module.exports = {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ['~/plugins/lang.js', '~/plugins/vue-cookies.js'],
+  plugins: ['~/plugins/lang.js', '~/plugins/vue-cookies.js', '~/plugins/vuelidate.js'],
   /*
    ** Nuxt.js dev-modules
    */
